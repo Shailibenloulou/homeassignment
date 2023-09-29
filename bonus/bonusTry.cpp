046593fd256c69b6dc8689a2860d59ad0dabc670
שילי בן לולו <shaili@g.jct.ac.il>
2023-09-29 16:08:12 +0300

    bonus

---
+++ bonus/bonusTry.cpp
@@ -0,0 +1,3 @@
+//https://docs.opencv.org/3.4/d5/db5/tutorial_laplace_operator.html
+//i read about the laplacian filter based on laplace theory. its actually really cool.
+//i download the openCv library but didn't manage to add it to visual studio for some reasons.
